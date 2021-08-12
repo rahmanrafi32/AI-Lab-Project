bfe34d228fd3bbbbb29d1382525bebcfe1e83ee4
rahmanrafi32 <rafi.rahman03@yahoo.com>
2021-08-12 19:11:49 +0600

    project complete

---
+++ main.py
@@ -0,0 +1,64 @@
+import pyttsx3
+import datetime
+import pyjokes
+import pywhatkit
+import wikipedia
+import speech_recognition as sr
+
+engine = pyttsx3.init()
+listener = sr.Recognizer()
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+speak('This is your virtual assistant. What can i do for you?')
+
+def sayTime():
+    time = datetime.datetime.now().strftime('It is: %I:%M:%S')
+    speak(time)
+
+def sayDate():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month)
+    day = int(datetime.datetime.now().day)
+    speak("today is:")
+    speak(day)
+    speak(month)
+    speak(year)
+
+def takeCommand():
+    with sr.Microphone() as source:
+        print("Listening...")
+        listener.pause_threshold = 1
+        voice = listener.listen(source)
+    try:
+        command = listener.recognize_google(voice, language='en-in')
+    except Exception as e:
+        speak('Sorry didnt get you. Say that again')
+        return 'None'
+    return command
+
+def runAssistant():
+    command = takeCommand()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        speak('playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        sayTime()
+    elif 'date' or 'what is the date' in command:
+        sayDate()
+    elif 'who is' in command:
+        person = command.replace('who is', '')
+        info = wikipedia.summary(person, 2)
+        speak(info)
+    elif 'are you single' in command:
+        speak('I am in a relationship with wifi')
+    elif 'joke' in command:
+        speak(pyjokes.get_joke())
+    else:
+        speak('Please say the command again.')
+        
+runAssistant()
+
